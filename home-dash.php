00ac71cecbd82e1d0ebdf2efe6801bd7fb89b0b0
Justine Bouin <112966252+alt-ju@users.noreply.github.com>
2024-02-18 18:14:10 +0100

    build : update home-dash.php

--- home-dash.php
+++ home-dash.php
@@ -6,45 +6,50 @@ include "includes/pages/header.php";
 ;?>
 
 
-<div>
-    <img src="" alt="">
+<div class="avatar-nav">
+    <img src="assets/img/avatar.jpeg" alt="">
     <h1>Nom Prénom</h1>
-    <div>
-        <p><a href="#">Paramètres</a></p>
-    </div>
-    <div>
-        <p><a href="#">Grand Angle</a></p>
-    </div>
-    <div>
-        <p><a href="#">Collaborateurs</a></p>
-    </div>
+    <ul>
+        <li><a href="#">Paramètres</a></li>
+        <li><a href="#">Grand Angle</a></li>
+        <li><a href="#">Collaborateurs</a></li>
+    </ul>
 </div>
 
-<div>
-    <div>
-        <p><a href="#">Liste des expositions</a></p>
-        <p><a href="#">Liste des artistes</a></p>
-        <p><a href="#">Implantation</a></p>
-    </div>
-    <div>
-        <div id="calendar">
+<div class="quick-nav">
+    <ul>
+        <li><a href="#">Liste des expositions</a></li>
+        <li><a href="#">Liste des artistes</a></li>
+        <li><a href="#">Implantation</a></li>
+    </ul>
+</div>
 
-        </div>
+<div class="main-nav">
+    <div id="calendar">
+    
+    </div>
+    <div class="add-buttons">
+        <input class="dash" type="button" value="Ajouter une exposition">
+        <input class="dash" type="button" value="Ajouter un artiste">
+        <input class="dash" type="button" value="Ajouter une oeuvre">
         <div>
-            
+            <input class="dash" type="button" value="Plan de l'exposition">
+            <input class="short dash" type="button" value="+">
         </div>
     </div>
-    <div>
+</div>
 
+<div class="art-state">
+    <h2>Etat des oeuvres :</h2>
+    <div>
+        <div class="first-art">
+             <img src="assets/img/freud_strawberries.jpg" alt="">
+             <p>id_oeuvre</p>
+             <p>Livrée</p>
+        </div> 
     </div>
 </div>
 
-<div>
 
-</div>
-
-<div>
-
-</div>
 
 
