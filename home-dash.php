eddeacb3de863b51b8ef125dda4feba89595c754
Justine Bouin <112966252+alt-ju@users.noreply.github.com>
2024-02-20 15:23:48 +0100

    commit before carrousel

--- home-dash.php
+++ home-dash.php
@@ -16,22 +16,15 @@ include "includes/pages/header.php";
     </ul>
 </div>
 
-<div class="quick-nav">
-    <ul>
-        <li><a href="#">Liste des expositions</a></li>
-        <li><a href="#">Liste des artistes</a></li>
-        <li><a href="#">Implantation</a></li>
-    </ul>
-</div>
-
 <div class="main-nav">
     <div id="calendar">
     
     </div>
     <div class="add-buttons">
-        <input class="dash" type="button" value="Ajouter une exposition">
-        <input class="dash" type="button" value="Ajouter un artiste">
-        <input class="dash" type="button" value="Ajouter une oeuvre">
+        <input class="dash long" type="button" value="Liste des expositions">
+        <input class="dash long" type="button" value="Liste des artistes">
+        <input class="dash long" type="button" value="Listes des oeuvres">
+        <input class="dash long" type="button" value="Implantation">
         <div>
             <input class="dash" type="button" value="Plan de l'exposition">
             <input class="short dash" type="button" value="+">
@@ -41,12 +34,122 @@ include "includes/pages/header.php";
 
 <div class="art-state">
     <h2>Etat des oeuvres :</h2>
-    <div>
-        <div class="first-art">
-             <img src="assets/img/freud_strawberries.jpg" alt="">
-             <p>id_oeuvre</p>
-             <p>Livrée</p>
-        </div> 
+    <div class="art-state-div">
+        <div class="art-state-content">
+            <div class="art">
+                <a href="#">
+                    <div>
+                        <div class="first-art">
+                            <img src="assets/img/freud_strawberries.jpg" alt="">
+                            <p>id_oeuvre</p>
+                            <p>Livrée</p>
+                        </div> 
+                    </div>
+                </a>
+            </div>
+            <div class="art">
+                <a href="#">
+                    <div>
+                        <div class="first-art">
+                            <img src="assets/img/freud_strawberries.jpg" alt="">
+                            <p>id_oeuvre</p>
+                            <p>Livrée</p>
+                        </div> 
+                    </div>
+                </a>
+            </div>
+            <div class="art">
+                <a href="#">
+                    <div>
+                        <div class="first-art">
+                            <img src="assets/img/freud_strawberries.jpg" alt="">
+                            <p>id_oeuvre</p>
+                            <p>Livrée</p>
+                        </div> 
+                    </div>
+                </a>
+            </div>
+            <div class="art">
+                <a href="#">
+                    <div>
+                        <div class="first-art">
+                            <img src="assets/img/freud_strawberries.jpg" alt="">
+                            <p>id_oeuvre</p>
+                            <p>Livrée</p>
+                        </div> 
+                    </div>
+                </a>
+            </div>
+            <div class="art">
+                <a href="#">
+                    <div>
+                        <div class="first-art">
+                            <img src="assets/img/freud_strawberries.jpg" alt="">
+                            <p>id_oeuvre</p>
+                            <p>Livrée</p>
+                        </div> 
+                    </div>
+                </a>
+            </div>
+            <div class="art">
+                <a href="#">
+                    <div>
+                        <div class="first-art">
+                            <img src="assets/img/freud_strawberries.jpg" alt="">
+                            <p>id_oeuvre</p>
+                            <p>Livrée</p>
+                        </div> 
+                    </div>
+                </a>
+            </div>
+            <div class="art">
+                <a href="#">
+                    <div>
+                        <div class="first-art">
+                            <img src="assets/img/freud_strawberries.jpg" alt="">
+                            <p>id_oeuvre</p>
+                            <p>Livrée</p>
+                        </div> 
+                    </div>
+                </a>
+            </div>
+            <div class="art">
+                <a href="#">
+                    <div>
+                        <div class="first-art">
+                            <img src="assets/img/freud_strawberries.jpg" alt="">
+                            <p>id_oeuvre</p>
+                            <p>Livrée</p>
+                        </div> 
+                    </div>
+                </a>
+            </div>
+            <div class="art">
+                <a href="#">
+                    <div>
+                        <div class="first-art">
+                            <img src="assets/img/freud_strawberries.jpg" alt="">
+                            <p>id_oeuvre</p>
+                            <p>Livrée</p>
+                        </div> 
+                    </div>
+                </a>
+            </div>
+            <div class="art">
+                <a href="#">
+                    <div>
+                        <div class="first-art">
+                            <img src="assets/img/freud_strawberries.jpg" alt="">
+                            <p>id_oeuvre</p>
+                            <p>Livrée</p>
+                        </div> 
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="arrow">
+            <a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg></a>
+        </div>
     </div>
 </div>
 
