b8570958170811820358474b9b12ccc4a36cfe3b
Justine Bouin <112966252+alt-ju@users.noreply.github.com>
2024-02-27 13:46:15 +0100

    changes finishing nav ready to merge

--- home-dash.php
+++ home-dash.php
@@ -5,30 +5,12 @@ include "includes/pages/header.php";
 include "includes/pages/navbarr.php"
 ;?>
 
-<!-- <div class="avatar-nav">
-    <img src="assets/img/avatar.jpeg" alt="">
-    <h1>Nom Prénom</h1>
-    <ul>
-        <li><a href="#">Paramètres</a></li>
-        <li><a href="#">Grand Angle</a></li>
-        <li><a href="#">Collaborateurs</a></li>
-    </ul>
-</div>
 
 <div class="main-nav">
     <div id="calendar">
     
     </div>
-    <div class="add-buttons">
-        <input class="dash long" type="button" value="Liste des expositions">
-        <input class="dash long" type="button" value="Liste des artistes">
-        <input class="dash long" type="button" value="Listes des oeuvres">
-        <input class="dash long" type="button" value="Implantation">
-        <div>
-            <input class="dash" type="button" value="Plan de l'exposition">
-            <input class="short dash" type="button" value="+">
-        </div>
-    </div>
+    
 </div>
 
 <div class="art-state">
@@ -153,7 +135,7 @@ include "includes/pages/navbarr.php"
             <a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg></a>
         </div>
     </div>
-</div> -->
+</div> 
 </main>
 </body>
 
