7b44a061346140480961edfe374943a388f70746
MahSafavi <143615455+MahSafavi@users.noreply.github.com>
2024-03-05 09:54:28 +0100

    add php code

---
+++ config/pdo.php
@@ -0,0 +1,17 @@
+<?php
+define ("DBHOST", "127.0.0.1"); 
+define ("DBUSER", "root");
+define ("DBPASS", "");
+define ("DBNAME", "grand angle");
+
+$dsn = "mysql:dbname=".DBNAME.";host=".DBHOST;
+
+try {
+    $db = new PDO($dsn, DBUSER, DBPASS);
+    $db->exec("SET NAMES utf8");
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+}
+catch(PDOException $e)
+    {die("Erreur de connexion à la BDD".$e->getMessage());} 
+    
+;?>
