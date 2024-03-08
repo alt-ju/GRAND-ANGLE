3e2064d61eb0c8d5f9536c40ee560e41c307fd99
Justine Bouin <112966252+alt-ju@users.noreply.github.com>
2024-03-08 16:58:15 +0100

    build: start php add oeuvre unique

--- add-oeuvre-unique.php
+++ add-oeuvre-unique.php
@@ -6,17 +6,72 @@ include 'includes/pages/header.php';
 include 'includes/pages/nav-head.php';
 include 'includes/pages/navbarr.php';
 
+function filtrage($data) {
+    $data = stripslashes($data);
+    $data = trim($data);
+    $data = strip_tags($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+function numInt($data) {
+     $data = preg_match('/^[0-9]+$/', $data);
+     return $data;
+}
+
+if(!empty($_POST)) {
+    if(isset($_POST["libelle"], $_POST["imgOeuvre"], $_POST["type"], $_POST["artiste"], $_POST["exposition"], $_POST["position"])
+    && !empty($_POST["libelle"]) && !empty($_POST["imgOeuvre"]) && !empty($_POST["type"]) && !empty($_POST["artiste"]) && !empty($_POST["exposition"]) && !empty($_POST["position"])
+    ){
+        $libelle = filtrage($_POST["libelle"]);
+
+        if(strlen($libelle) >= 150) {
+            $error["libelle"] = "Le libellé est trop long";
+        }
+
+        if(!numInt($_POST["hauteur"])) {
+            $error = "La hauteur ne doit contenir que des numéros";
+        }
+
+        if(!numInt($_POST["largeur"])) {
+            $error = "La largeur ne doit contenir que des numéros";
+        }
+
+        if(!numInt($_POST["profondeur"])) {
+            $error = "La profondeur ne doit contenir que des numéros";
+        }
+
+        if(!numInt($_POST["poids"])) {
+            $error = "Le poids ne doit contenir que des numéros";
+        }
+
+        if(!numInt($_POST["prix"])) {
+            $error = "Le prix ne doit contenir que des numéros";
+        }
+
+        require_once 'config/pdo.php';
+        $sql = "INSERT INTO "
+
+
+
+
+
+
+    }
+}
+
 ;?>
 
 <div class="gestion gestion-add-oeuvre">
 <div class="oeuvre-unique-contain">
 
     <div class="oeuvre-unique-infos col">
-        <form action="">
+        <form action="" method="POST">
 
             <div class="div-libelle-add-oeuvre">
                 <label for="libelle">Libellé de l'oeuvre :</label>
                 <input type="text" name="libelle" id="libelle" class="field-add-oeuvre">
+                <span>*</span>
             </div>
 
             <div class="div-photo-add-oeuvre">
@@ -24,7 +79,8 @@ include 'includes/pages/navbarr.php';
                     <button><svg  viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg></button>
                 </div>
                 <div class="container-img-oeuvre">
-                    <img src="assets/img/imgvide.webp" alt="">
+                    <span>*</span>
+                    <input type="file" name="imgOeuvre" id="imgOeuvre" accept="image/*">
                     <div class="add-img-plus">
                         <svg  viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
                     </div>
@@ -36,47 +92,69 @@ include 'includes/pages/navbarr.php';
 
             <div class="div-infos-oeuvre">
                 
-                <div class="input-nom">
-                    <input type="text" name="nom" id="nom" value="" placeholder="Nom">
-                </div>
-
-                <div class="input-prenom">
-                    <input type="text" name="nom" id="nom" value="" placeholder="Prénom">
-                </div>
-
-                <div class="input-dimensions multi">
-                    <div class="haut-oeuvre-add position">
-                        <label for="hauteur">Hauteur :</label>
-                        <input type="text" class="dim-add-oeuvre" name="hauteur" id="hauteur" value="">
-                    </div>
-                    <div class="larg-oeuvre-add position">
-                        <label for="largeur">Largeur :</label>
-                        <input type="text" class="dim-add-oeuvre" name="largeur" id="largeur" value="">
-                    </div>
-                    <div class="prof-oeuvre-add position">
-                        <label for="profondeur">Profondeur :</label>
-                        <input type="text" class="dim-add-oeuvre" name="profondeur" id="profondeur" value="">
-                    </div>
-                </div>
-
-                <div class="input-infos-supp multi">
-                    <div class="info-poids-add">
-                        <input type="text"  class="infos-supp-input" name="poids" id="poids" value="" placeholder="Poids">
-                    </div>
-                    <div class="info-duree-add">
-                        <input type="text"  class="infos-supp-input" name="duree" id="duree" value="" placeholder="Durée">
-                    </div>
-                </div>
-
                 <div class="select-type-add-oeuvre">
-                    <select name="select-type" id="select-type">
-                        <option value="">Type d'oeuvres</option>
+                    <select name="artiste" id="artiste">
+                        <option value="">Artiste</option>
                     </select>
+                    <span>*</span>
                     <div class="add-type-plus">
                         <a href="#">
                             <svg  viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
                             <span>Créer</span>
                         </a>
+                    </div>
+                </div>
+            
+                <div class="select-type-add-oeuvre">
+                    <select name="type" id="type">
+                        <option value="">Type d'oeuvres</option>
+                    </select>
+                    <span>*</span>
+                    <div class="add-type-plus">
+                        <a href="#">
+                            <svg  viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
+                            <span>Créer</span>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="select-type-add-oeuvre">
+                    <select name="exposition" id="exposition">
+                        <option value="">Exposition</option>
+                    </select>
+                    <span>*</span>
+                    <div class="add-type-plus">
+                        <a href="#">
+                            <svg  viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
+                            <span>Créer</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="select-type-add-oeuvre">
+                    <select name="position" id="position">
+                        <option value="">Position</option>
+                    </select>
+                    <span>*</span>
+                </div>
+                
+                <div class="input-dimensions multi">
+                    <div class="haut-oeuvre-add position">
+                        <input type="text" class="dim-add-oeuvre" name="hauteur" id="hauteur" value="" placeholder="Hauteur">
+                    </div>
+                    <div class="larg-oeuvre-add position">
+                        <input type="text" class="dim-add-oeuvre" name="largeur" id="largeur" value="" placeholder="Largeur">
+                    </div>
+                    <div class="prof-oeuvre-add position">
+                        <input type="text" class="dim-add-oeuvre" name="profondeur" id="profondeur" value="" placeholder="Profondeur">
+                    </div>
+                </div>
+
+                <div class="input-infos-supp multi">
+                    <div class="info-poids-add">
+                        <input type="text"  class="dim-add-oeuvre" name="poids" id="poids" value="" placeholder="Poids">
+                    </div>
+                    <div class="info-prix-add">
+                        <input type="text"  class="dim-add-oeuvre" name="prix" id="prix" value="" placeholder="Prix">
                     </div>
                 </div>
                 
